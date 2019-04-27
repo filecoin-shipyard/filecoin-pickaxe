fed6ff1fc29fa897e7d700f1db7965432da0fb30
Jim Pick <jim@backblend.com>
2019-04-26 23:10:15 -0700

    Use import API

--- importBundle.js
+++ importBundle.js
@@ -1,15 +1,33 @@
+import fs from 'fs'
 import React, { useState, useEffect } from 'react'
 import { Box } from 'ink'
+import Filecoin from 'filecoin-api-client'
 import GroupContext from './groupContext'
 import ExitNow from './inkExitNow'
 import WatchForExitKey from './inkWatchForExitKey'
 
+const fc = Filecoin()
+
 function Import ({ group }) {
-  const [counter, setCounter] = useState(0)
-  const [done, setDone] = useState()
+  const [file, setFile] = useState()
+  const [cid, setCid] = useState()
+  const [error, setError] = useState()
 
   useEffect(() => {
-    let count = counter
+    let unmounted
+    const files = group.collaboration.shared.value()
+    if (files.length === 0) return
+    const lastFile = files[files.length - 1]
+    const match = lastFile.match(/^Add: \d+ (.*)$/)
+    if (!match) return
+    const file = match[1]
+    setFile(file)
+    const data = fs.createReadStream(file)
+    fc.client.import(data)
+      .then(cid => !unmounted && setCid(cid.toString()))
+      .catch(error => !unmounted && setError(error))
+
+    /*
     function doWork () {
       count++
       setCounter(count)
@@ -20,12 +38,33 @@ function Import ({ group }) {
       }
     }
     setTimeout(doWork, 0)
+    */
+    return () => { umounted = true }
   }, [])
 
+  if (!file) {
+    return (
+      <Box>
+        No file
+      </Box>
+    )
+  }
+  if (error) {
+    return (
+      <Error>
+        Error: {error.toString()}
+      </Error>
+    )
+  }
   return (
-    <Box>
-      Counter: {counter}
-      {done && <ExitNow />}
+    <Box flexDirection="column">
+      <Box>
+        File: {file}
+      </Box>
+      <Box>
+        CID: {cid}
+        {cid && <ExitNow />}
+      </Box>
     </Box>
   )
 }
